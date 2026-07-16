6c49e8a51a118f4eace18a7141f8c53a718031cb
Paniz <panizbolouri1401@gmail.com>
2026-07-16 15:20:45 +0330

    a

---
+++ src/GameMain.java
@@ -0,0 +1,46 @@
+import database.DatabaseManager;
+import ui.GamePanel;
+import ui.HowToPlayPanel;
+import ui.LeaderboardPanel;
+import ui.LoginPanel;
+import ui.MainMenu;
+import ui.RegisterPanel;
+import ui.SettingsPanel;
+import javax.swing.*;
+import java.awt.*;
+
+public class GameMain {
+    public static void main(String[] args) {
+        DatabaseManager.initialize();
+
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Chicken Invaders");
+            frame.setSize(800, 600);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setResizable(false);
+            frame.setLocationRelativeTo(null);
+
+            CardLayout cardLayout = new CardLayout();
+            JPanel mainPanel = new JPanel(cardLayout);
+
+            LoginPanel loginPanel = new LoginPanel(mainPanel, cardLayout);
+            RegisterPanel registerPanel = new RegisterPanel(mainPanel, cardLayout);
+            MainMenu mainMenuPanel = new MainMenu(mainPanel, cardLayout);
+            GamePanel gamePanel = new GamePanel(mainPanel, cardLayout);
+            LeaderboardPanel leaderboardPanel = new LeaderboardPanel(mainPanel, cardLayout);
+            HowToPlayPanel howToPlayPanel = new HowToPlayPanel(mainPanel, cardLayout);
+            SettingsPanel settingsPanel = new SettingsPanel(mainPanel, cardLayout);
+
+            mainPanel.add(loginPanel, "LoginPage");
+            mainPanel.add(registerPanel, "RegisterPage");
+            mainPanel.add(mainMenuPanel, "MainMenu");
+            mainPanel.add(gamePanel, "GamePage");
+            mainPanel.add(leaderboardPanel, "LeaderboardPage");
+            mainPanel.add(howToPlayPanel, "HowToPlayPage");
+            mainPanel.add(settingsPanel, "SettingsPage");
+
+            frame.add(mainPanel);
+            frame.setVisible(true);
+        });
+    }
+}
