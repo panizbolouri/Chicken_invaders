070a79bb5e50dd32495b9bb309649692b5cf8059
Paniz <panizbolouri1401@gmail.com>
2026-07-17 22:03:30 +0330

    2

--- src/model/DefaultPlane.java
+++ src/model/DefaultPlane.java
@@ -16,5 +16,11 @@ public class DefaultPlane extends Plane {
         if (image != null) {
             g.drawImage(image, x, y, 100, 100, null);
         }
+        if (isShieldActive()) {
+            g.setColor(new Color(0, 200, 255, 100));
+            g.fillOval(x - 10, y - 10, 120, 120);
+            g.setColor(new Color(0, 255, 255, 200));
+            g.drawOval(x - 10, y - 10, 120, 120);
+        }
     }
 }
